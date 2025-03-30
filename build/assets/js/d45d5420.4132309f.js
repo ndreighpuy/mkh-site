08bbfaa0e2dafdc80e8a18ae3209e90d316844d7
ndreighpuy <mkhadaffy1805@gmail.com>
2025-03-30 10:32:30 +0700

    upd 3

---
+++ build/assets/js/d45d5420.4132309f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[301],{2993:e=>{e.exports=JSON.parse('{"tag":{"label":"webscrape","permalink":"/tags/webscrape","allTagsPath":"/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/tags/webscrape","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
